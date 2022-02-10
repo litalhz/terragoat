cc85adc8e9be6fbefda024e7deeec83066471530
litalhz <48403406+litalhz@users.noreply.github.com>
2022-02-10 16:25:46 +0200

    Update provider.tf

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -11,3 +11,15 @@ terraform {
   backend "azurerm" {
   }
 }
+
+provider "azurerm2" {
+  subscription_id = var.subscription_id
+  version = ">= 2.0.0"
+  features {}
+}
+
+terraform {
+  required_version = ">=0.12.0"
+  backend "azurerm2" {
+  }
+}
