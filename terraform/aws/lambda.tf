599d7f7a25fb27c257534acefc89f0878df258c1
litalhz <48403406+litalhz@users.noreply.github.com>
2022-04-28 14:10:30 +0300

    Update lambda.tf

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -36,3 +36,22 @@ resource "aws_lambda_function" "analysis_lambda" {
     }
   }
 }
+
+resource "aws_lambda_function" "analysis_lambda2" {
+  # lambda have plain text secrets in environment variables
+  filename      = "resources/lambda_function_payload.zip"
+  function_name = "${local.resource_prefix.value}-analysis"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  source_code_hash = "${filebase64sha256("resources/lambda_function_payload.zip")}"
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      access_key = "AKIAIOSFODNN7EXAMPLE"
+      secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    }
+  }
+}
